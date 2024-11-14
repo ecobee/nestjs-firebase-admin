b5b5295fcee838d68f8c6cbe9c7d6635ede8e7a0
Vikram Venkataramanan <vikram.v@ecobee.com>
2024-11-14 11:30:09 -0500

    reformat

--- lib/firebase-admin.module.spec.ts
+++ lib/firebase-admin.module.spec.ts
@@ -1,38 +1,38 @@
-import { Test } from '@nestjs/testing'
-import { FirebaseAdminModule } from './firebase-admin.module'
+import { Test } from '@nestjs/testing';
+import { FirebaseAdminModule } from './firebase-admin.module';
 
 describe('FirebaseAdminModule', () => {
-	describe('forRoot', () => {
-		beforeEach(async () => {
-			await Test.createTestingModule({
-				imports: [
-					FirebaseAdminModule.forRoot({
-						projectId: 'nestjs-firebase-admin-for-test',
-					}),
-				],
-			}).compile()
-		})
+  describe('forRoot', () => {
+    beforeEach(async () => {
+      await Test.createTestingModule({
+        imports: [
+          FirebaseAdminModule.forRoot({
+            projectId: 'nestjs-firebase-admin-for-test',
+          }),
+        ],
+      }).compile();
+    });
 
-		it('should create', () => {
-			expect(FirebaseAdminModule).toBeDefined()
-		})
-	})
+    it('should create', () => {
+      expect(FirebaseAdminModule).toBeDefined();
+    });
+  });
 
-	describe('forRootAsync', () => {
-		beforeEach(async () => {
-			await Test.createTestingModule({
-				imports: [
-					FirebaseAdminModule.forRootAsync({
-						useFactory: () => ({
-							projectId: 'nestjs-firebase-admin-for-test',
-						}),
-					}),
-				],
-			}).compile()
-		})
+  describe('forRootAsync', () => {
+    beforeEach(async () => {
+      await Test.createTestingModule({
+        imports: [
+          FirebaseAdminModule.forRootAsync({
+            useFactory: () => ({
+              projectId: 'nestjs-firebase-admin-for-test',
+            }),
+          }),
+        ],
+      }).compile();
+    });
 
-		it('should create', () => {
-			expect(FirebaseAdminModule).toBeDefined()
-		})
-	})
-})
+    it('should create', () => {
+      expect(FirebaseAdminModule).toBeDefined();
+    });
+  });
+});
